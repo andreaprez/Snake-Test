495d4ffb1a125e06d57f2d5e17def1a631c053d4
Andrea Pérez <andreaprezp@gmail.com>
2023-09-01 16:03:31 +0200

    Creation of PoolManager
    - Created PoolManager.cs class to handle multiple pools of objects in a scene. For now, there are 2 types of pools: Sound and Food.
    - Created Prefabs folder and Prefabs for Food and Sound gameobjects.
    - Updated SoundManager.cs to use the pool manager for the AudioSource instances when playing a sound.

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+    public static PoolManager instance;
+    [SerializeField] private List<ObjectPool> listOfPools;
+    
+    void Awake() {
+        instance = this;
+    }
+
+    void Start() {
+        if (listOfPools != null) {
+            for (int i = 0; i < listOfPools.Count; i++) {
+                FillPool(listOfPools[i]);
+            }
+        }
+    }
+
+    void FillPool(ObjectPool pool) {
+        for (int i = 0; i < pool.amount; i++) {
+            var tmpInstance = Instantiate(pool.prefab, pool.container.transform);
+            tmpInstance.SetActive(false);
+            tmpInstance.transform.position = pool.container.transform.position;
+            pool.objects.Add(tmpInstance);
+        }
+    }
+    
+    public GameObject GetPoolObject(ObjectPoolType type) {
+        ObjectPool pool = GetPoolByType(type);
+        List<GameObject> poolObjects = pool.objects;
+
+        if (poolObjects != null && poolObjects.Count > 0)
+        {
+            GameObject selectedObj = null;
+            for (int i = 0; i < poolObjects.Count; i++) {
+                if (!poolObjects[i].activeInHierarchy)
+                {
+                    selectedObj = poolObjects[i];
+                }
+            }
+            
+            // Move the selected object to the start of the pool to keep the pool organised.
+            if (selectedObj != null) {
+                poolObjects.Remove(selectedObj);
+                poolObjects.Insert(0, selectedObj);
+                return selectedObj;
+            }
+
+            return poolObjects[poolObjects.Count - 1]; // If every object in the pool is active, it will return the one that has been active for longer.
+        }
+        return null;
+    }
+    
+    public ObjectPool GetPoolByType(ObjectPoolType type) {
+        for (int i = 0; i < listOfPools.Count; i++) {
+            if (type.Equals(listOfPools[i].type)) {
+                return listOfPools[i];
+            }
+        }
+        return null;
+    }
+
+    public void ResetPoolObject(GameObject obj, ObjectPoolType type) {
+        obj.SetActive(false);
+        
+        ObjectPool pool = GetPoolByType(type);
+        if (pool != null) {
+            obj.transform.position = pool.container.transform.position;
+            
+            // Move the cleared object to the end of the pool to keep the pool organised.
+            pool.objects.Remove(obj);
+            pool.objects.Add(obj);
+        }
+    }
+}
+
+[Serializable]
+public class ObjectPool
+{
+    public ObjectPoolType type;
+    public GameObject prefab;
+    public GameObject container;
+    public int amount;
+
+    public List<GameObject> objects = new List<GameObject>();
+}
+
+public enum ObjectPoolType
+{
+    Audio,
+    Food
+}
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -97,7 +97,7 @@ public class Snake : MonoBehaviour {
         if (gridMoveTimer >= gridMoveTimerMax) {
             gridMoveTimer -= gridMoveTimerMax;
 
-            //SoundManager.PlaySound(SoundManager.Sound.SnakeMove);
+            SoundManager.PlaySound(SoundManager.Sound.SnakeMove);
 
             SnakeMovePosition previousSnakeMovePosition = null;
             if (snakeMovePositionList.Count > 0) {
