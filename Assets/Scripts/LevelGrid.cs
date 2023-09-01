6b0385c04508c84edd0490e46daeb054171b8eac
Andrea Pérez <andreaprezp@gmail.com>
2023-09-01 16:18:21 +0200

    Use pool manager to spawn food
    Updated LevelGrid.cs to use a pool from PoolManager.cs

--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -39,14 +39,14 @@ public class LevelGrid {
             foodGridPosition = new Vector2Int(Random.Range(0, width), Random.Range(0, height));
         } while (snake.GetFullSnakeGridPositionList().IndexOf(foodGridPosition) != -1);
 
-        foodGameObject = new GameObject("Food", typeof(SpriteRenderer));
-        foodGameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.i.foodSprite;
+        foodGameObject = PoolManager.instance.GetPoolObject(ObjectPoolType.Food);
         foodGameObject.transform.position = new Vector3(foodGridPosition.x, foodGridPosition.y);
+        foodGameObject.SetActive(true);
     }
 
     public bool TrySnakeEatFood(Vector2Int snakeGridPosition) {
         if (snakeGridPosition == foodGridPosition) {
-            Object.Destroy(foodGameObject);
+            PoolManager.instance.ResetPoolObject(foodGameObject, ObjectPoolType.Food);
             SpawnFood();
             Score.AddScore();
             return true;
